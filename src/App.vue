7d57c9faf84bdbcc3e1d9bb5802ebd67da3abc30
hossein <hkhalili@ucla.edu>
2024-01-29 15:24:37 -0800

    Revert "add physics"
    This reverts commit 1e0d6b7251aae54afac7c55c56b72edbc6452f91.

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,8 @@
     <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
     <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
     <a-sky color="#ECECEC"></a-sky>
-      <!-- Camera -->
-      <a-camera position="0 1.6 0"></a-camera>
-
-      <!-- Hand tracking controllers -->
-      <a-entity id="left-hand" oculus-touch-controls="hand: left" hand-controls="hand: left" visible="true"></a-entity>
-      <a-entity id="right-hand" oculus-touch-controls="hand: right" hand-controls="hand: right" visible="true"></a-entity>
-
-      <!-- Cubes -->
-      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" dynamic-body></a-box>
-      <a-box position="1 0.5 -3" rotation="0 45 0" color="#EF2D5E" dynamic-body></a-box>
+      <a-entity id="leftHand" hand-tracking-controls="hand: left;"></a-entity>
+      <a-entity id="rightHand" hand-tracking-controls="hand: right;"></a-entity>
 
     </a-scene>
   </div>
@@ -23,7 +15,7 @@
 
 <script>
 import 'aframe'; // Import A-Frame
-import 'aframe-physics-system'; // If you plan to use physics
+
 export default {
   name: 'App',
   methods: {
